aee47fc143d28de22abb2a1313ff3d16ad4ac1fe
darshik <darshikbambhroliya@gmail.com>
2026-05-18 14:09:23 +0530

    Stop-watch project completed

---
+++ Stop-Watch/src/App.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setRunning] = useState(false);
+  useEffect(() => {
+    let intervalId: number;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+  // Hours calculation
+  const hours = Math.floor(time / 360000);
+
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = time % 100;
+  const start = () => {
+    setRunning(true);
+  };
+  const stop = () => {
+    setRunning(false);
+  };
+  const reset = () => {
+    setTime(0);
+  };
+  return (
+    <div>
+      <p className="stopwatch-time">
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </p>
+
+      <div className="stopwatch-buttons">
+        <button className="stopwatch-button" onClick={start}>
+          Start
+        </button>
+
+        <button className="stopwatch-button" onClick={stop}>
+          Stop
+        </button>
+
+        <button className="stopwatch-button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
